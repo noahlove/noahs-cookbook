34a7906375454916df5b4e5c73631dd4aaebb276
Noah Love <noah.love@columbia.edu>
2024-08-24 15:03:07 -0400

    Updated logo

--- app/(website)/about/about.js
+++ app/(website)/about/about.js
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <Container>
       <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
-        About Noah's Cookbook
+        About Noah&apos;s Cookbook
       </h1>
       <div className="text-center">
         <p className="text-lg">A personal journey through flavors and memories.</p>
@@ -15,27 +15,27 @@ export default function About() {
         <div className="relative w-64 h-64">
           <Image
             src="/img/noahs-cookbook-icon.png"
-            alt="Noah's Cookbook Icon"
-            layout="fill"
-            objectFit="contain"
+            alt="Noah&apos;s Cookbook Icon"
+            fill
+            style={{ objectFit: "contain" }}
           />
         </div>
       </div>
       <div className="prose mx-auto mt-14 text-center dark:prose-invert">
         <p>
-          Welcome to Noah's Cookbook, a cozy corner of the internet where I share my culinary adventures and favorite recipes.
+          Welcome to Noah&apos;s Cookbook, a cozy corner of the internet where I share my culinary adventures and favorite recipes.
         </p>
         <p>
           Inspired by the elegance and warmth of Quince restaurant, this blog is my digital kitchen - a place to store memories, flavors, and the joy of cooking.
         </p>
         <p>
-          Here, you'll find a collection of recipes I've discovered, adapted, and fallen in love with. From comforting home-cooked meals to ambitious culinary experiments, each post is a slice of my gastronomic journey.
+          Here, you&apos;ll find a collection of recipes I&apos;ve discovered, adapted, and fallen in love with. From comforting home-cooked meals to ambitious culinary experiments, each post is a slice of my gastronomic journey.
         </p>
         <p>
-          This blog isn't about monetization or becoming a culinary influencer. It's simply my way of documenting the dishes that have left an impression on me, the restaurants that have inspired me, and the food stories I want to remember.
+          This blog isn&apos;t about monetization or becoming a culinary influencer. It&apos;s simply my way of documenting the dishes that have left an impression on me, the restaurants that have inspired me, and the food stories I want to remember.
         </p>
         <p>
-          Whether you're here to find your next cooking project or just to share in my love for good food, I'm glad you've stopped by. Feel free to explore, and I hope you find something that inspires you to create your own delicious memories.
+          Whether you&apos;re here to find your next cooking project or just to share in my love for good food, I&apos;m glad you&apos;ve stopped by. Feel free to explore, and I hope you find something that inspires you to create your own delicious memories.
         </p>
         <p>
           Happy cooking!
--- components/navbar.js
+++ components/navbar.js
@@ -16,7 +16,6 @@ export default function Navbar(props) {
       label: "About",
       href: "/about"
     },
-
   ];
 
   const rightmenu = [
@@ -24,7 +23,6 @@ export default function Navbar(props) {
       label: "Archive",
       href: "/archive"
     },
-
   ];
 
   const mobilemenu = [...leftmenu, ...rightmenu];
@@ -62,52 +60,44 @@ export default function Navbar(props) {
                   <Link href="/" className="w-28 dark:hidden">
                     {props.logo ? (
                       <Image
-                        {...urlForImage(props.logo)}
+                        src="/img/logo.svg"
+                        width={640}
+                        height={400}
                         alt="Logo"
                         priority={true}
                         sizes="(max-width: 640px) 100vw, 200px"
+                        
                       />
                     ) : (
                       <span className="block text-center">
-                        Stablo
+                        Noah's Cookbook
                       </span>
                     )}
                   </Link>
                   <Link href="/" className="hidden w-28 dark:block">
                     {props.logoalt ? (
                       <Image
-                        {...urlForImage(props.logoalt)}
+                        src="/img/logo.svg"
+                        width={640}
+                        height={400}
                         alt="Logo"
                         priority={true}
                         sizes="(max-width: 640px) 100vw, 200px"
                       />
                     ) : (
                       <span className="block text-center">
-                        Stablo
+                        Noah's Cookbook
                       </span>
                     )}
                   </Link>
                   <Disclosure.Button
                     aria-label="Toggle Menu"
                     className="ml-auto rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 md:hidden ">
-                    <svg
+                    <img
+                      src="/img/logo.svg"
                       className="h-6 w-6 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24">
-                      {open && (
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                        />
-                      )}
-                      {!open && (
-                        <path
-                          fillRule="evenodd"
-                          d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                        />
-                      )}
-                    </svg>
+                      alt="Menu Toggle"
+                    />
                   </Disclosure.Button>
                 </div>
 
